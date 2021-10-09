22d1b6e9b3d7f577c6b64bd29fb13a66b825f035
Axle <alex.vanleyen@gmail.com>
2021-10-09 17:05:55 -0400

    ref: moved mocks to own module

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,17 +1,6 @@
 import { ApolloServer } from 'apollo-server';
 import typeDefs from './schema';
-
-const mocks = {
-  Query: () => ({
-    books: () => [...new Array(6)]
-  }),
-  Book: () => ({
-    id: () => 'z23e35',
-    title: () => 'Love and Peace',
-    year: () => 1982,
-    description: () => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc semper finibus augue, id suscipit est vestibulum quis. Praesent pharetra egestas tincidunt. Sed fringilla tristique urna, ac ullamcorper orci suscipit ac. Donec vitae elementum est. Duis semper neque vitae pulvinar pellentesque. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam porttitor fringilla felis, id imperdiet urna gravida eu. Donec mattis erat a est iaculis gravida. In mi lectus, condimentum et orci sit amet, consectetur ullamcorper massa. Phasellus tincidunt egestas quam. Nunc non risus elit. Nunc pharetra leo eget eros eleifend tincidunt. Ut elementum dolor nunc, non cursus dolor consectetur nec. Ut convallis a nibh id dictum. Fusce aliquam elit ac rutrum lacinia.'
-  })
-};
+import mocks from './mocks';
 
 const server = new ApolloServer({ typeDefs, mocks });
 
