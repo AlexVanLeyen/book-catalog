611ea95b7adaab31fd5167c520c8ac7754e731ce
Axle <alex.vanleyen@gmail.com>
2021-10-10 00:56:37 -0400

    feat: added query loading and error handling

--- client/src/pages/BookCatalog/BookCatalog.tsx
+++ client/src/pages/BookCatalog/BookCatalog.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import { useHistory } from 'react-router-dom';
+import Alert from '@mui/material/Alert';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Container from '@mui/material/Container';
+import LinearProgress from '@mui/material/LinearProgress';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import CatalogItem from '../../components/CatalogItem';
@@ -11,7 +13,7 @@ import { TBook } from '../../types/book.type';
 import { useQuery, gql } from '@apollo/client';
 
 export const BOOKS = gql`
-query Books {
+query GetBooks {
   books {
     id
     title
@@ -23,24 +25,29 @@ query Books {
 
 const BookCatalog: React.FunctionComponent<{books: Array<TBook>}> = () => {
   const history = useHistory();
-  const { data } = useQuery(BOOKS);
-
+  const { data, loading, error } = useQuery(BOOKS);
   function onClickBook (id: string): void {
     history.push(`/books/${id}`);
   };
 
   return (
       <main>
+        { loading && <LinearProgress /> }
         <Container sx={{ py: 8 }} maxWidth="md">
-        {data && data.books && data.books.length > 0
-          ? <Stack spacing={4}>{data.books.map((book: any) => (
-              <CatalogItem key={book.id} item={book} onClick={() => onClickBook(book.id)} />
-          ))}</Stack>
-          : <Card>
-              <CardContent sx={{ textAlign: 'center' }}>
-                <Typography><FormattedMessage id="app.noBooks" /></Typography>
-              </CardContent>
-            </Card>
+        { error
+          ? <Alert severity="error">An error occurred while requesting for your books.</Alert>
+          : <>
+            {data && data.books && data.books.length > 0
+              ? <Stack spacing={4}>{data.books.map((book: any) => (
+                  <CatalogItem key={book.id} item={book} onClick={() => onClickBook(book.id)} />
+              ))}</Stack>
+              : <Card>
+                  <CardContent >
+                    <Typography sx={{ textAlign: 'center' }}><FormattedMessage id="app.noBooks" /></Typography>
+                  </CardContent>
+                </Card>
+            }
+            </>
         }
         </Container>
       </main>
